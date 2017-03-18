140eed8a4ceb1b84d5dc8b9170820be13303c866
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2017-03-18 23:14:30 +0100

    Changed soures of warp10 config

--- src/main/java/io/warp10/spark/Warp10Spark.java
+++ src/main/java/io/warp10/spark/Warp10Spark.java
@@ -1,23 +1,36 @@
 package io.warp10.spark;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+
+import org.apache.spark.SparkFiles;
 
 import io.warp10.WarpConfig;
 import io.warp10.script.WarpScriptLib;
 
 public class Warp10Spark {
-  public static void init() throws IOException {
-    if (null != System.getProperty(WarpConfig.WARP10_CONFIG)) {
-      WarpConfig.setProperties(new InputStreamReader(Warp10Spark.class.getClassLoader().getResourceAsStream(System.getProperty(WarpConfig.WARP10_CONFIG))));
-    } else {
-      WarpConfig.setProperties((String) null);
+  public static void init() {
+    try {
+      if (null != System.getProperty(WarpConfig.WARP10_CONFIG)) {
+        InputStream in = Warp10Spark.class.getClassLoader().getResourceAsStream(System.getProperty(WarpConfig.WARP10_CONFIG));
+        
+        if (null == in) {
+          in = new FileInputStream(SparkFiles.get(System.getProperty(WarpConfig.WARP10_CONFIG)));
+        }
+        WarpConfig.safeSetProperties(new InputStreamReader(in));
+      } else {
+        WarpConfig.safeSetProperties((String) null);
+      }
+      
+      //
+      // Register extensions
+      //
+      
+      WarpScriptLib.registerExtensions();      
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-    
-    //
-    // Register extensions
-    //
-    
-    WarpScriptLib.registerExtensions();
   }
 }
