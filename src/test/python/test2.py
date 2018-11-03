db5db54c9712b3b2459e9a663b651bd82162e25e
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2018-11-03 10:03:34 +0100

    Clean up. Fixed build.gradle copyright

--- src/test/python/test2.py
+++
@@ -1,9 +0,0 @@
-from py4j.java_gateway import java_import
-from pyspark import SparkContext
-
-sc = SparkContext()
-java_import(sc._gateway.jvm,"io.warp10.spark.PySparkLauncher")
-
-func = sc._gateway.jvm.PySparkLauncher()
-params = {'inFile': 'file:///tmp/test.in', 'outFile': 'file:///tmp/test.out'}
-print func.testPySparkLauncher(params)
