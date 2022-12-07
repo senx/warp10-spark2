b54901e8904b2b94935ff5ca09f4a01a50dfd02d
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2022-12-07 14:48:30 +0100

    Initial commit of WarpScriptUDF

---
+++ src/main/java/io/warp10/spark/WarpScriptUDF.java
@@ -0,0 +1,44 @@
+//
+//   Copyright 2022  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package io.warp10.spark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.warp10.spark.common.SparkUtils;
+import io.warp10.spark.common.WarpScriptAbstractFunction;
+
+public class WarpScriptUDF<R> extends WarpScriptAbstractFunction {
+
+  public R call(Object... args) throws Exception {
+    synchronized(this) {
+      setCode(args[0].toString());
+
+      List<Object> stackInput = new ArrayList<Object>(args.length - 1);
+      for (int i = 1; i < args.length; i++) {
+        stackInput.add(args[i]);
+      }
+      List<Object> stackResult = executor.exec(stackInput);
+
+      if (1 == stackResult.size()) {
+        return (R) SparkUtils.toSpark(stackResult.get(0));
+      } else {
+        return (R) SparkUtils.toSpark(stackResult);
+      }
+    }
+  }
+}
