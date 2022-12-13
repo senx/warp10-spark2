abf7fba6e372fd0945b7260982eb4eff7c911d20
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2022-12-13 15:40:41 +0100

    Added method saveAsNewAPIHadoopFile which is a wrapper that ensures Warp10Spark.init is called

--- src/main/java/io/warp10/spark/common/SparkUtils.java
+++ src/main/java/io/warp10/spark/common/SparkUtils.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2018-2022  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -15,7 +15,12 @@
 //
 package io.warp10.spark.common;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -24,28 +29,25 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import com.google.common.base.Charsets;
-
-import io.warp10.continuum.gts.GTSDecoder;
-import io.warp10.continuum.gts.GTSHelper;
-import io.warp10.continuum.gts.GTSWrapperHelper;
-import io.warp10.continuum.store.thrift.data.GTSWrapper;
-import io.warp10.continuum.store.thrift.data.Metadata;
-import io.warp10.script.WarpScriptException;
-
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.spark.SparkFiles;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Row;
-import org.apache.thrift.TDeserializer;
-import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TCompactProtocol;
 
+import com.google.common.base.Charsets;
+
+import io.warp10.script.WarpScriptException;
+import io.warp10.spark.Warp10Spark;
 import scala.Product;
 import scala.collection.Iterator;
 
 public class SparkUtils {
+
   public static Object fromSpark(Object o) {
+
     if (null == o || o instanceof NullWritable) {
       return null;
     } else if (o instanceof String) {
@@ -60,14 +62,14 @@ public class SparkUtils {
       return ((Number) o).doubleValue();
     } else if (o instanceof Product) {
       Product prod = (Product) o;
-      
+
       List<Object> list = new ArrayList<Object>();
       scala.collection.Iterator<Object> iter = prod.productIterator();
-      
+
       while(iter.hasNext()) {
         list.add(fromSpark(iter.next()));
       }
-      
+
       return list;
     } else if (o instanceof List) {
       List<Object> l = new ArrayList<Object>();
@@ -76,7 +78,7 @@ public class SparkUtils {
       }
       return l;
     } else if (o instanceof Row) {
-      List<Object> l = new ArrayList<Object>(((Row) o).size());      
+      List<Object> l = new ArrayList<Object>(((Row) o).size());
       Row row = (Row) o;
       for (int i = 0; i < row.size(); i++) {
         l.add(fromSpark(row.get(i)));
@@ -111,19 +113,19 @@ public class SparkUtils {
       return o;
     } else if (o instanceof List) {
       ArrayList<Object> l = new ArrayList<Object>();
-      
+
       for (Object elt: (List) o) {
         l.add(toSpark(elt));
       }
-      
+
       return l;
     } else if (o instanceof Map) {
       HashMap<Object,Object> map = new HashMap<Object,Object>();
-      
+
       for (Entry<Object,Object> entry: ((Map<Object,Object>) o).entrySet()) {
         map.put(toSpark(entry.getKey()), toSpark(entry.getValue()));
       }
-      
+
       return map;
     } else {
       return o;
@@ -144,18 +146,18 @@ public class SparkUtils {
     StringBuffer scriptSB = new StringBuffer();
     InputStream fis = null;
     BufferedReader br = null;
-    try {      
+    try {
       File f = new File(SparkFiles.get(warpscriptFile));
       if (!f.exists()) {
         fis = SparkUtils.class.getClassLoader().getResourceAsStream(warpscriptFile);
       } else {
         fis = new FileInputStream(f);
       }
-      
+
       if (null == fis) {
         throw new IOException("WarpScript file '" + warpscriptFile + "' could not be found.");
       }
-      
+
       br = new BufferedReader(new InputStreamReader(fis, Charsets.UTF_8));
 
       while (true) {
@@ -173,6 +175,20 @@ public class SparkUtils {
     }
 
     return scriptSB.toString();
+  }
 
+  public static void saveAsNewAPIHadoopFile(JavaRDD rdd, String path, String kc, String vc, String ofc, Map<String,String> cf) throws Exception {
+    // We MUST call Warp10Spark.init() so we ensure that extensions are loaded and hence we can find any OutputFormat which
+    // may come as part of an extension (i.e. HFile)
+    Warp10Spark.init();
+    JavaPairRDD prdd = JavaPairRDD.fromJavaRDD(rdd);
+    Class keyClass = SparkUtils.class.forName(kc);
+    Class valueClass = SparkUtils.class.forName(vc);
+    Class outputFormatClass = SparkUtils.class.forName(ofc);
+    Configuration conf = new Configuration();
+    for (Entry<String,String> entry: cf.entrySet()) {
+      conf.set(entry.getKey(), entry.getValue());
+    }
+    prdd.saveAsNewAPIHadoopFile(path, keyClass, valueClass, outputFormatClass, conf);
   }
 }
