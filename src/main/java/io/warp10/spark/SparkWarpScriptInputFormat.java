44cb1b19c39f3513a5104084b2d847acbd9d21af
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2023-01-31 16:11:34 +0100

    Added Warp 10 init

--- src/main/java/io/warp10/spark/SparkWarpScriptInputFormat.java
+++ src/main/java/io/warp10/spark/SparkWarpScriptInputFormat.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2018-2023  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -27,22 +27,27 @@ import io.warp10.hadoop.WarpScriptInputFormat;
 import io.warp10.spark.common.SparkUtils;
 
 public class SparkWarpScriptInputFormat extends WarpScriptInputFormat {
+
+  static {
+    Warp10Spark.init();
+  }
+
   @Override
   public InputStream getWarpScriptInputStream(String originalFilePath) throws IOException {
     String filename = Paths.get(originalFilePath).getFileName().toString();
 
     File f = new File(SparkFiles.get(filename));
-    
+
     InputStream fis = null;
-    
+
     if (!f.exists()) {
       fis = SparkUtils.class.getClassLoader().getResourceAsStream(filename);
     }
-    
+
     if (null == fis) {
       fis = new FileInputStream(f);
     }
-    
+
     if (null == fis) {
       throw new IOException("WarpScript file '" + originalFilePath + "' could not be found.");
     }
